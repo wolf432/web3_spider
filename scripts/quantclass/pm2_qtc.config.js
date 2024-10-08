34f76e08d5c85e3804128a5f34096fe102f904c5
LiNan <wolf430@163.com>
2024-10-08 21:46:07 +0800

    添加量化论坛pm2文件

--- scripts/quantclass/pm2_qtc.config.js
+++ scripts/quantclass/pm2_qtc.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [{
     name: "web3_qtc_sync_content",
     script: "sync_content.py",
-    interpreter: "/home/ubuntu/miniconda3/envs/web3_spider/bin/python",
+    interpreter: "/root/miniconda3/envs/web3_spider/bin/python",
     log_file: "logs/pm2_qtc.log",
     out_file: "logs/pm2_qtc_out.log",
     error_file: "logs/pm2_qtc_error.log",
