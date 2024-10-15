08042a700c17bc9df1d39cdcbfdfd95e2e486825
LiNan <wolf430@163.com>
2024-10-15 11:55:07 +0800

    修改pm2配置

--- scripts/quantclass/pm2_qtc.config.js
+++ scripts/quantclass/pm2_qtc.config.js
@@ -2,10 +2,6 @@ module.exports = {
   apps : [{
     name: "web3_qtc_sync_content",
     script: "sync_content.py",
-    interpreter: "/root/miniconda3/envs/web3_spider/bin/python",
-    log_file: "logs/pm2_qtc.log",
-    out_file: "logs/pm2_qtc_out.log",
-    error_file: "logs/pm2_qtc_error.log",
-    max_size: '100M',
+    interpreter: "/root/miniconda3/envs/web3_spider/bin/python"
   }]
 };
